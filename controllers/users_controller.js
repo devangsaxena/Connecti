3200de47e1be26b3b14b24d4ab1a3de9b5ebc0a3
devangsaxena <devangsaxena2018@gmail.com>
2020-11-23 23:29:54 +0530

    auth part-2

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -9,6 +9,9 @@ module.exports.profile=function(req,res){
 
 //render the sign up page
 module.exports.signUp=function(req,res){
+    if(req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
     return res.render('user_sign_up',{
         title: "Connecti | Sign Up"
     })
@@ -16,6 +19,9 @@ module.exports.signUp=function(req,res){
 
 // render the sign in page
 module.exports.signIn=function(req,res){
+    if(req.isAuthenticated()){
+       return res.redirect('/users/profile');
+    }
     return res.render('user_sign_in',{
         title: "Connecti | Sign In"
     })
@@ -43,5 +49,11 @@ module.exports.create = function(req,res){
 }
 //sign in and craete a session for user
 module.exports.createSession= function(req,res){
-    //TODO
+    return res.redirect('/');
+}
+
+
+module.exports.destroySession= function(req,res){
+    req.logout();
+    return res.redirect('/');
 }
--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app=express();
 const port=8000;
 const expressLayouts= require('express-ejs-layouts');
 const db=require('./config/mongoose');
+//used for session cookie
+const session= require('express-session');
+const passport=require('passport');
+const passportLocal=require('./config/passport-local-strategy');
+
+const MongoStore=require('connect-mongo')(session);
+
 app.use(express.urlencoded());
 app.use(cookieParser());
 
@@ -13,12 +20,39 @@ app.set('layout extractStyles',true);
 app.set('layout extractScripts',true);
 app.use(express.static('./assets'));
 // use express router
-app.use('/',require('./routes'));
+
 
 //set up the view engine
 app.set('view engine','ejs');
 app.set('views','./views');
 
+//mongostore is used to store the session cookie in db
+
+app.use(session({
+    name:'Connecti',
+    secret: 'blahsomething',
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: (1000*60*100)
+    },
+    store: new MongoStore(
+        {
+            mongooseConnection : db,
+            autoRemove: 'disabled'
+        },
+        function(err){
+           console.log(err || 'connect-mongodb setup ok'); 
+        }
+    )
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(passport.setAuthenticatedUser);
+
+app.use('/',require('./routes'));
 app.listen(port,function(err){
     if(err){
         console.log(`Error in running server: ${err}`);
