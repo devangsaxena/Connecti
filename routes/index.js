7a3fa6b52bf22a58f98c45df26a9f9ec8d0c2119
devangsaxena <devangsaxena2018@gmail.com>
2020-11-23 12:35:35 +0530

    added users controller

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,11 @@ const express= require('express');
 
 const router=express.Router();
 const homeController =require('../controllers/home_controller');
-router.get('/',homeController.home);
 
+
+
+
+router.get('/',homeController.home);
+router.use('/users',require('./users'));
 
 module.exports=router;
