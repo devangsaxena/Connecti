f6a531632eb0b6c6a705598b8d7d4837c6a4353b
devangsaxena <devangsaxena2018@gmail.com>
2020-11-23 19:49:53 +0530

    mongodb connected

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express=require('express');
 const app=express();
 const port=8000;
 const expressLayouts= require('express-ejs-layouts');
+const db=require('./config/mongoose');
 app.use(expressLayouts);
 //extract style and scripts from sub pages into the layout
 app.set('layout extractStyles',true);
