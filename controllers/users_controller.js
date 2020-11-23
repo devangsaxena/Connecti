d33c663d59ceb543f3647266450986e4e8b38034
devangsaxena <devangsaxena2018@gmail.com>
2020-11-23 21:17:48 +0530

    auth-1 done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,6 +1,47 @@
+const User = require("../models/user");
+
 module.exports.profile=function(req,res){
     //res.end('<h1>User Profile</h1>');
     return res.render('user_profile',{
         title:"User Profile"
     });
+}
+
+//render the sign up page
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title: "Connecti | Sign Up"
+    })
+}
+
+// render the sign in page
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title: "Connecti | Sign In"
+    })
+}
+
+//get the sign up data
+module.exports.create = function(req,res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+
+    User.findOne({email: req.body.email}, function(err, user){
+        if(err){console.log('error in finding the user in signing up');return}
+
+        if(!user){
+            User.create(req.body, function(err,user){
+                if(err){console.log('error in finding the user in signing up');return}
+
+                return res.redirect('/users/sign-in')
+            })
+        }else{
+            return res.redirect('back');
+        }
+    })
+}
+//sign in and craete a session for user
+module.exports.createSession= function(req,res){
+    //TODO
 }
