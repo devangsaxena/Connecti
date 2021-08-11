5d6f17ecfe0005ef6df3d3ae15c5f6bd88c37898
Devang Saxena <46219105+devangsaxena@users.noreply.github.com>
2021-08-11 15:31:09 +0530

    Update posts_controller.js

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -2,21 +2,9 @@ const Post=require('../models/post');
 const Comment=require('../models/comment');
 const Like = require('../models/like');
 
-// Create a post
-// module.exports.create=function(req, res){
-//     Post.create({
-//         content: req.body.content,
-//         user: req.user._id
-//     }, function(err, post){
-//         if(err){
-//             console.log('Error in creating a post');
-//             return;
-//         }
-//         return res.redirect('back');
-//     });
-// }
 
-// Create a post by Async Await
+
+// Created a post by Async Await
 module.exports.create= async function(req, res){
     try {
         // await Post.create({
